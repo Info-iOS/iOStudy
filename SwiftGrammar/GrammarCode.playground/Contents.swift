4bacac740822b9d8c6f4d18d71094979064c4504
junha <goodjunha@gmail.com>
2023-04-11 18:05:20 +0900

    코드 파일 추가

---
+++ SwiftGrammar/GrammarCode.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+class Man {
+    var name: String?
+    var age: Int?
+
+    init(name: String?, age: Int?) {
+            self.name = name
+            self.age = age
+    }
+}
+
+let a = Man(name: "ssss", age: 19)
